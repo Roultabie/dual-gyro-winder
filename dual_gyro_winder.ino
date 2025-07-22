bbacc25c768348f1ef24326ed7ca5d5177566a38
Daniel Douat <Roultabie@users.noreply.github.com>
2025-07-22 21:11:27 +0200

    Added first version of script via upload

---
+++ dual_gyro_winder.ino
@@ -0,0 +1,114 @@
+//**************************************************************
+// Commande de moteur pas-à-pas unipolaire 4 fils
+// tiptopboards.com 05/12/2013
+// Mise à jour du 28/07/2025
+// Refactorisation du code
+// Ajout de la gestion de la rotation des moteurs simultanés
+// Ajout d'un section dédiée à la personnalisation des variables
+// Ajout de documentation
+// Driver ULN2003 et moteur réducté au 1:64 de type 28BYJ-48
+// 64 pas par tour, 4 phases, angle de 5.625° selon les spécifications du moteur
+// Démultiplication 1:64 pour ce moteur réducté mécaniquement
+// 360° / 5.625° * 64 = 4096 angles avec la démultiplication
+// 360° / 5.625° * 64  * 4 bobines / 2 bipolaire = 2048 step / tour
+//
+//**************************************************************
+//Inclure la librairie stepper.h
+#include <Stepper.h>
+#define STEPS 100
+
+const int selecteurLeft = 2; // Numéro de la broche à laquelle est connecté le bouton poussoir 1
+const int selecteurRight = 3; // Numéro de la broche à laquelle est connectée le bouton poussoir 2
+const int ledSwitch = 4; // Numéro de la broche à laquelle est connecté la led du bouton pour allumer / éteindre le système
+const int nbPas = 4096; // Nombre de pas demandés au moteur, une rotation complète avec 2048 pas (1 tour environ 4.5sec)
+const int nbClignLed = 3; // Nombre de clignotement de la led après deux rotations (permet d'arrêter le système moteurs bien placés)
+const int attenteLed = 1000; // Durée en ms entre les deux clignotements de la led.
+const int attenteTour = 1000; // Durée en ms entre une rotation horaire et une rotation anti horaire
+const int series = 3; // Nombre de révolutions faites avant une pause
+const int speed = 300; // Vitesse de 300 (max) réduire ce chiffre pour un mouvement plus lent du moteur pas à pas
+//100 permet d'avoir un couple élevé >300 le moteur vibre sans tourner
+const unsigned long pause = 300000; // Pause entre deux séries (en millisecondes)
+
+//Créer une instance de la classe stepper
+//Le moteur (fils 1 2 3 4) est branché sur les sorties 9 10 11 12 de l'Arduino (et sur GND, +V)
+Stepper small_stepper1(STEPS, 9, 11, 10, 12);  // Sens horaire
+Stepper small_stepper2(STEPS, 5, 7, 6, 8); // Sens horaire
+
+int compteur = 0;
+int motors;
+
+void setup() {
+  Serial.begin(9600);  // 9600 bps
+  Serial.println("Lancement du mouvement gyroscopique");
+  pinMode(ledSwitch, OUTPUT);  // Declare le Pin 5 comme sortie pour la LED et indique que la broche led est une sortie :
+  pinMode(selecteurLeft, INPUT); // indique que la broche bouton est une entrée :
+  pinMode(selecteurRight, INPUT); // indique que la broche bouton est une entrée :
+}
+
+// Démarrage de la boucle
+void loop() {
+  digitalWrite(ledSwitch, HIGH);  // On allume la led
+  delay(100);
+  // lit l'état du sélecteur 3 positions et extrapole le ou les moteurs allumés dans la variable motors (0 -> les deux moteurs, 1 gauche, 2 droite)
+  motors = (digitalRead(selecteurLeft) == LOW && digitalRead(selecteurRight) == LOW) ? 0 : (digitalRead(selecteurLeft) == HIGH) ? 1 : 2;
+
+  // On définit la vitesse des moteurs
+  Serial.println("Réglage de la vitesse des moteurs");
+  small_stepper1.setSpeed(speed);
+  small_stepper2.setSpeed(speed);  //Vitesse de 300 (max) réduire ce chiffre pour un mouvement plus lent
+                                   //100 permet d'avoir un couple élevé >300 le moteur vibre sans tourner
+
+  // Lancement d'un série de rotations
+  if (compteur < series) {
+    // Rotation simultanée des moteurs quand le switch est au centre
+    Serial.println("Rotation des moteurs");
+    Serial.println(motors);
+    Serial.println("Série no :");
+    Serial.println(compteur);
+    int st;  // Pour simulation de rotation simultanée des moteurs
+    // Rotation au sens horaire
+    Serial.println("Rotation horaire");
+    for (st = 1; st <= nbPas; st++) {
+      if (motors == 0 || motors == 1) {
+        small_stepper1.step(-1);
+      }
+      if (motors == 0 || motors == 2) {
+        small_stepper2.step(-1);
+      }
+    }
+
+    // Pause entre une rotation horaire et une rotation anti horaire
+    delay(attenteTour);
+
+    // Rotation au sens anti horaire
+    Serial.println("Rotation anti horaire");
+    for (st = 1; st <= nbPas; st++) {
+      if (motors == 0 || motors == 1) {
+        small_stepper1.step(1);
+      }
+      if (motors == 0 || motors == 2) {
+        small_stepper2.step(1);
+      }
+    }
+
+    // Pause après une rotation horaire / anti horaire permettant d'arrêter physiquement le système, moteurs bien placés
+    Serial.println("Pause entre deux révolutions");
+    for (int led = 0; led < nbClignLed; led++) {
+      // Clignotement de la led
+      digitalWrite(ledSwitch, LOW);
+      delay(attenteLed);
+      digitalWrite(ledSwitch, HIGH);
+      delay(attenteLed);
+    }
+    compteur++;  //Ajoute 1 au Compteur
+  } else {
+    Serial.println("Pause à la fin d'une série");
+    // On s'assure que la LED est allumée
+    digitalWrite(ledSwitch, HIGH);
+    Serial.println(pause);
+    delay(pause);
+    // On réinitialise le compteur de séries
+    Serial.println("Rénitialisation du compteur");
+    compteur = 0;
+  }
+}
